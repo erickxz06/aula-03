9bc4c817a7a0c241379711300c12b6c9f56a28c4
erick <erickdcs2006@gmail.com>
2025-04-01 09:48:32 -0300

    adicionei outro exercício

---
+++ exercício02.py
@@ -0,0 +1,9 @@
+numero1 = float(input("digite o primeiro número: "))
+numero2 = float(input("digite o segundo número: "))
+adicao = numero1 + numero2
+subtracao = numero1 - numero2
+divisao = numero1 / numero2
+multiplicalao = numero1 * numero2
+
+print(f"a soma dos dois números é {adicao}, a subtração dos dois números é {subtracao}")
+print(f"a divisão dos dois números é {divisao:.2f}, a multiplicação dos dois números é {multiplicalao}")
