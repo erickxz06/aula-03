3aacc1516b1f19f9c273cf9b0b06cbdaaa20c3ee
erick <erickdcs2006@gmail.com>
2025-04-01 08:51:45 -0300

    terminei o primeiro exércicio

---
+++ exercício01.py
@@ -0,0 +1,5 @@
+nome = input("digite seu nome: ")
+idade = int(input("digite sua idade: "))
+salário = float(input("digite seu salário aqui: "))
+
+print(f"Seu nome é {nome}, você têm {idade} anos, seu salário é {salário}R$")
