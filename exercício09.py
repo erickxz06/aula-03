037930527f216b2860e003cf9f645b870b41af1b
erick <erickdcs2006@gmail.com>
2025-04-08 09:32:50 -0300

    exercício09

---
+++ exercício09.py
@@ -0,0 +1,33 @@
+numero = int(input("Número: "))
+
+if numero == 1:
+    print(f"janeiro")
+elif numero == 2:
+    print(f"fevereiro")
+
+elif numero == 3:
+    print(f"março")
+elif numero == 4:
+    print(f"abril")
+
+elif numero == 5:
+    print(f"maio")
+elif numero == 6:
+    print(f"junho")
+
+elif numero == 7:
+    print(f"julho")
+elif numero == 8:
+    print(f"agosto")
+
+elif numero == 9:
+    print(f"setembro")
+elif numero == 10:
+    print(f"outubro")
+
+elif numero == 11:
+    print(f"novembro")
+elif numero == 12:
+    print(f"dezembro")
+else:
+    print("numero invalido")
